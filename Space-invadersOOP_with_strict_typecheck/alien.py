fcd4f5e74be62bd975ff3aa23f892e62517136dc
nitslaszlo <nitslaszlo@gmail.com>
2024-02-02 23:21:02 +0100

    up

--- Space-invadersOOP_with_strict_typecheck/alien.py
+++ Space-invadersOOP_with_strict_typecheck/alien.py
@@ -1,32 +1,43 @@
+from typing import Any
+
 import pygame
 
+
 class Alien(pygame.sprite.Sprite):
-	def __init__(self,color,x,y):
-		super().__init__()
-		file_path = '../graphics/' + color + '.png'
-		self.image = pygame.image.load(file_path).convert_alpha()
-		self.rect = self.image.get_rect(topleft = (x,y))
+    def __init__(self, color: str, x: int, y: int):
+        super().__init__()
+        file_path: str = "graphics/" + color + ".png"
+        self.image: pygame.Surface = pygame.image.load(file_path).convert_alpha()
+        self.rect: pygame.rect.Rect = self.image.get_rect(topleft=(x, y))
+        self.value: int = 0
 
-		if color == 'red': self.value = 100
-		elif color == 'green': self.value = 200
-		else: self.value = 300
+        if color == "red":
+            self.value = 100
+        elif color == "green":
+            self.value = 200
+        else:
+            self.value = 300
 
-	def update(self,direction):
-		self.rect.x += direction
+    def update(self, *args: Any, **kwargs: dict[str, Any]) -> None:
+        super().update(*args, **kwargs)
+        self.rect.x += int(args[0])
+
 
 class Extra(pygame.sprite.Sprite):
-	def __init__(self,side,screen_width):
-		super().__init__()
-		self.image = pygame.image.load('../graphics/extra.png').convert_alpha()
-		
-		if side == 'right':
-			x = screen_width + 50
-			self.speed = - 3
-		else:
-			x = -50
-			self.speed = 3
+    def __init__(self, side: str, screen_width: int):
+        super().__init__()
+        self.image = pygame.image.load("graphics/extra.png").convert_alpha()
 
-		self.rect = self.image.get_rect(topleft = (x,80))
+        if side == "right":
+            x = screen_width + 50
+            self.speed = -3
+        else:
+            x = -50
+            self.speed = 3
 
-	def update(self):
-		self.rect.x += self.speed
+        self.rect = self.image.get_rect(topleft=(x, 80))
+
+    #def update(self):
+    def update(self, *args: Any, **kwargs: dict[str, Any]) -> None:
+        super().update(*args, **kwargs)
+        self.rect.x += self.speed
